2f696674872392a975b2b9ffc86848a683b3feb1
MUGARURA AYEBARE A. ANDRE <andre_mugarura@taylor.edu>
2025-11-07 01:46:42 -0500

    #SVELTE_1 submission

---
+++ frontend/src/lib/components/User.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+  let { user, onDelete } = $props();
+</script>
+
+<div>
+  <h3>{user.username}</h3>
+  <p>ID: {user.id}</p>
+  <button onclick={() => onDelete(user.id)}>
+    Delete
+  </button>
+</div>
+<hr />
