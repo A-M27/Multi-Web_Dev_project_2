4f9256a12feb484dbc32ef2b60945175e068d868
MUGARURA AYEBARE A. ANDRE <andre_mugarura@taylor.edu>
2025-11-17 14:41:16 -0500

    Update for the search and pagination combination in user.py with the profeessor

--- api/v1/endpoints/users.py
+++ api/v1/endpoints/users.py
@@ -1,18 +1,21 @@
 from fastapi import APIRouter, HTTPException, Depends, status
-from sqlmodel import Session, select, func
+from sqlmodel import Session, select, func, col
 from db.models import User
 from db.session import get_session
 
 router = APIRouter()
 
 @router.get("/", status_code=status.HTTP_200_OK)
-def get_all_users(session: Session = Depends(get_session), crPage: int = 1, perPage: int = 10):
+def get_all_users(session: Session = Depends(get_session), crPage: int = 1, perPage: int = 10, userSearch:str=""):
     offset_value = (crPage - 1) * perPage
-    users_statement = select(User).offset(offset_value).limit(perPage)
-    users = session.exec(users_statement).all()
+    #users_statement = select(User).offset(offset_value).limit(perPage)
+    #users = session.exec(users_statement).all()
     count_statement = select(func.count(User.id))
     total_count = session.exec(count_statement).one()
-    return {"users": users, "total_count": total_count}
+    statement = select(User).where(col(User.username).ilike(f"%{userSearch}%")).offset(offset_value).limit(perPage)
+    allusers = session.exec(statement).all()
+    #return {"users": users, "total_count": total_count}
+    return {"users": allusers, "total_count": total_count}
 
 
 
